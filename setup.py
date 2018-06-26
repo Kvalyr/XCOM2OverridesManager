c52da71f356ac7f84f63656a67c97d32ec6295c8
Kvalyr <misc@valyr.net>
2018-06-26 02:01:51 -0700

    Fix typo. Flesh out setup.py a bit.

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 setup(
-    name='Ooerrides',
-    packages=find_packages(),
+    name='Overrides',
+    version='1.3',
+    description="A tool to clean, fix and manipulate XCOM2 Configuration files to support mods.",
+    url="https://github.com/Kvalyr/XCOM2OverridesManager",
+    packages=find_packages('Overrides'),
+    author='Kvalyr',
 )
